4b32a1fce21d811e646d7d7fa7380f7a0974d709
jkreuzweb <jan.krizek@mendelova-stredni.cz>
2026-04-30 17:10:31 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="cs">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SX416SNEYD"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-SX416SNEYD');
+        `}} />
+      </head>
       <body>{children}</body>
     </html>
   )
